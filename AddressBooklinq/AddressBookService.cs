1925cfad85f8128ef20f11fc067a1eec6a650ec9
sonal <sonalkarle01@gmail.com>
2021-03-16 21:46:21 +0530

    Ability to delete the contact

--- AddressBooklinq/AddressBookService.cs
+++ AddressBooklinq/AddressBookService.cs
@@ -1,10 +1,95 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
 
-namespace AddressBooklinq
+namespace AddressBookLinq
 {
-    class AddressBookService
+    public class AddressBookService
     {
+        private DataSet AddressBookDB = new DataSet("AddressBookService");
+        public DataTable AddressBook;
+        public DataTable CreateAddressBookTable()
+        {
+            AddressBook = new DataTable("AddressBook");
+            DataColumn column;
+            column = new DataColumn();
+            column.DataType = typeof(string);
+            column.ColumnName = "FirstName";
+            AddressBook.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = typeof(string);
+            column.ColumnName = "LastName";
+            AddressBook.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = typeof(string);
+            column.ColumnName = "Address";
+            AddressBook.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = typeof(string);
+            column.ColumnName = "City";
+            AddressBook.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = typeof(string);
+            column.ColumnName = "State";
+            AddressBook.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = typeof(string);
+            column.ColumnName = "Zip";
+            AddressBook.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = typeof(string);
+            column.ColumnName = "PhoneNumber";
+            AddressBook.Columns.Add(column);
+
+            column = new DataColumn();
+            column.DataType = typeof(string);
+            column.ColumnName = "Email";
+            AddressBook.Columns.Add(column);
+            AddressBookDB.Tables.Add(AddressBook);
+            return AddressBook;
+        }
+
+        public DataRow EditContactUsingName(string name, string Column, string data)
+        {
+            DataRow row = AddressBook.AsEnumerable().Where(person => person.Field<string>("FirstName") + " " + person.Field<string>("LastName") == name)
+                .FirstOrDefault();
+            row[Column] = data;
+            return row;
+        }
+
+        public DataRow AddContact(Person person)
+        {
+            AddressBook.Rows.Add(person.FirstName, person.LastName,
+                person.Address, person.City, person.State, person.Zip, person.PhoneNumber, person.Email);
+            return AddressBook.Rows[AddressBook.Rows.Count - 1];
+        }
+
+        public bool DeleteContact(string name)
+        {
+            DataRow row = AddressBook.AsEnumerable().Where(person => person.Field<string>("FirstName") + " " + person.Field<string>("LastName") == name)
+                .FirstOrDefault();
+            row.Delete();
+            return row.RowState.Equals(DataRowState.Detached);
+        }
+
+        public void printTable(DataTable dataTable)
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    Console.WriteLine(column.ColumnName + " : " + row[column] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
